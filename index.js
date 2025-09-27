953fc830a64596d642b3399f368802c2a3100d9f
abigaelseenoi <abigaelseenoi5@gmail.com>
2025-09-27 15:10:17 +0300

    add fetch

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+// fetchBooks is called automatically when index.html loads
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // return fetch so tests can access it
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((books) => renderBooks(books));
 }
 
+// this function is already provided for you in the lab
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// call fetchBooks so it runs when page loads
+document.addEventListener("DOMContentLoaded", fetchBooks);
+
